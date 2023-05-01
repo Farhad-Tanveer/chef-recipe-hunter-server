f510b8b22768fe18cd812081c4cc0f4e764f95c8
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-05-01 22:43:56 +0200

    added chefs data

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = 3000;
 
 app.use(cors());
 
+const chefsData = require("./data/chefs.json");
+
 app.get("/", (req, res) => {
   res.send("Chef recipe hunter is running");
+});
+
+app.get("/chefsData", (req, res) => {
+  res.send(chefsData);
 });
 
 app.listen(port, () => {
